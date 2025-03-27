2a935f8d9381667a3449ea34119479f5e8d4d9b3
tequ <git@tequ.dev>
2025-03-27 13:33:32 +0900

    add html metadata

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -2,6 +2,8 @@ import vikeReact from 'vike-react/config'
 import type { Config } from 'vike/types'
 
 const config: Config = {
+  title: 'XRPL/Xahau DEX Metrics',
+  description: 'A dashboard for the XRPL/Xahau DEX',
   extends: [vikeReact],
 }
 
